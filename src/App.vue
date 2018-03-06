42391f5a351c9f3f3bcec2587b82a50fb9797aea
glebec-v <gvorotilov@gmail.com>
2018-03-06 04:05:47 +0300

    continue with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+      <nav class="navbar navbar-toggleable-md navbar-light bg-light">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <router-link to="/" class="navbar-brand">
+              Vue.js course app
+          </router-link>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="nav navbar-nav mr-auto">
+                  <router-link tag="li" active-class="active" to="/" exact>
+                      <a class="nav-link">Home</a>
+                  </router-link>
+              </ul>
+          </div>
+      </nav>
+
+      <div id="app">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+
 export default {
   name: 'App'
 }
@@ -13,11 +34,6 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
