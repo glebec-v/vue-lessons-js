60e42f9047738093d84f4320959efc8ebb9513cb
glebec-v <gvorotilov@gmail.com>
2018-03-12 02:16:17 +0300

    home-task-4 started pagination

--- src/components/ListControls.vue
+++ src/components/ListControls.vue
@@ -1,15 +1,31 @@
 <template>
-    
+    <div>
+        <paginator-settings v-on:usersonpage="settingsHandler"></paginator-settings>
+        <span class="alert-info">{{ paginator.lines }}</span>
+    </div>
 </template>
 
 <script>
     const paginator = {}; // todo import
-    const paginatorSettings = {}; // todo import
+    import paginatorSettings from '@/components/PaginatorSettings'
     export default {
         name: "list-controls",
         components: {
             'paginator-controls': paginator,
             'paginator-settings': paginatorSettings
+        },
+        data() {
+            return {
+                paginator: {
+                    lines: 0,
+                    page: 0
+                }
+            }
+        },
+        methods: {
+            settingsHandler: function (eventData) {
+                this.paginator.lines = eventData.lines;
+            }
         }
     }
 </script>
